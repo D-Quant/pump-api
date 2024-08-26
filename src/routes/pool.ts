f5f013ed6c00809ebdc7013c0e520b2772bcd211
Matrix.Ye <initsysctrl@outlook.com>
2024-08-27 00:30:39 +0800

    api:pool info

--- src/routes/pool.ts
+++ src/routes/pool.ts
@@ -2,20 +2,34 @@ import Router from "koa-router";
 import {pumpEngine} from "../config";
 import {sendErrorResponse} from "../helpers/response";
 
-const axios = require('axios');
-
 const router = new Router();
 // 获取池子信息
-router.get('/pump_info', async (ctx) => {
+router.get('/info', async (ctx) => {
     // 代币地址mint
     const mint = ctx.query.mint as string;
-
-    const info = await pumpEngine.getPoolInfo(mint)
-    if (!info) {
-        sendErrorResponse(ctx, 501, 'can not find this BondingCurveAccount by mint mint')
+    if (!mint) {
+        sendErrorResponse(ctx, 501, 'token mint miss')
         return
     }
-    ctx.body = info
+    try {
+        const info = await pumpEngine.getPoolInfo(mint)
+        if (!info) {
+            sendErrorResponse(ctx, 501, 'can not find this BondingCurveAccount by mint mint')
+            return
+        }
+        ctx.body = {
+            'discriminator': info.discriminator.toString(),
+            'virtualTokenReserves': info.virtualTokenReserves.toString(),
+            'virtualSolReserves': info.virtualSolReserves.toString(),
+            'realTokenReserves': info.realTokenReserves.toString(),
+            'realSolReserves': info.realSolReserves.toString(),
+            'tokenTotalSupply': info.tokenTotalSupply.toString(),
+            'complete': info.complete.toString(),
+        }
+    } catch (e) {
+        sendErrorResponse(ctx, 501, `${e}`)
+        return
+    }
 })
 
 export default router;
